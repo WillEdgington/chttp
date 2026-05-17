40cf9f6d9bca09fa8c1c627d6219427169cbbd26
WillEdgington <willedge037@gmail.com>
2026-05-17 23:29:03 +0100

    test: (fs) add tests for mock binary png file

--- tests/test_fs.c
+++ tests/test_fs.c
@@ -18,12 +18,27 @@ void setup_test_html_mock() {
 
 void teardown_test_html_mock() { unlink("test_www/index.html"); }
 
+static const char mock_png_data[] = {'\x89', 'P',  'N',  'G',
+                                     '\0',   '\0', '\r', '\n'};
+static const size_t mock_png_size = sizeof(mock_png_data);
+
+void setup_test_binary_mock() {
+  FILE *f = fopen("test_www/image.png", "wb");
+  if (f != NULL) {
+    fwrite(mock_png_data, 1, mock_png_size, f);
+    fclose(f);
+  }
+}
+
+void teardown_test_binary_mock() { unlink("test_www/image.png"); }
+
 void test_html_file(Arena *a) {
   setup_test_html_mock();
 
   char *path = chttp_resolve_path("/index.html", "test_www", a);
-  ASSERT_PTR_NOT_NULL(path, "Should resolve valid file");
-  ASSERT_PTR_NOT_NULL(strstr(path, "test_www/index.html"), "Path mismatch");
+  ASSERT_PTR_NOT_NULL(path, "Should resolve valid html file");
+  ASSERT_PTR_NOT_NULL(strstr(path, "test_www/index.html"),
+                      "Path should not have mismatch");
 
   char *unsafe = chttp_resolve_path("/../../etc/passwords", "test_www", a);
   ASSERT_PTR_NULL(unsafe, "Should block directory traversal");
@@ -32,10 +47,29 @@ void test_html_file(Arena *a) {
   char *content = chttp_read_file(path, a, &size);
 
   ASSERT_PTR_NOT_NULL(content, "File should be readable");
-  ASSERT_INT_EQ(size, 16, "Size mismatch");
-  ASSERT_STR_EQ(content, "<h1>Testing</h1>", "Content mismatch");
+  ASSERT_INT_EQ(size, 16, "Size should not have mismatch");
+  ASSERT_STR_EQ(content, "<h1>Testing</h1>",
+                "Content should not have mismatch");
 
   teardown_test_html_mock();
+}
+
+void test_binary_file(Arena *a) {
+  setup_test_binary_mock();
+
+  char *path = chttp_resolve_path("/image.png", "test_www", a);
+  ASSERT_PTR_NOT_NULL(path, "Should resolve valid binary file");
+
+  size_t size;
+  char *content = chttp_read_file(path, a, &size);
+
+  ASSERT_PTR_NOT_NULL(content, "Binary file should be readable");
+  ASSERT_INT_EQ(size, mock_png_size, "Binary size should not have mismatch");
+
+  ASSERT(memcmp(content, mock_png_data, mock_png_size) == 0,
+         "Binary content should not be corrupted");
+
+  teardown_test_binary_mock();
 }
 
 int main() {
@@ -45,6 +79,7 @@ int main() {
 
   printf("\nRunning: %s...\n", __FILE__);
   test_html_file(&a);
+  test_binary_file(&a);
   test_summary();
 
   teardown_test_dir();
