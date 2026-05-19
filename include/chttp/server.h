99b1c8b4ee32cccf51c157abc7136950874cad57
WillEdgington <willedge037@gmail.com>
2026-05-19 13:34:26 +0100

    refactor: (server) server module sends request conditional http responses

--- include/chttp/server.h
+++ include/chttp/server.h
@@ -10,7 +10,7 @@ typedef struct {
 
 // port < 0 defaults to 8080, *pub_dir of NULL defaults to "."
 HttpConfig chttp_config_init(int port, const char *pub_dir);
-void chttp_handle_connection(int client_fd);
-int chttp_listen_and_serve(HttpConfig config);
+int chttp_handle_connection(int client_fd, const char *pub_dir);
+int chttp_listen_and_serve(HttpConfig *config);
 
 #endif
--- src/server.c
+++ src/server.c
@@ -1,4 +1,7 @@
 #include "chttp/server.h"
+#include "chttp/handler.h"
+#include "chttp/http.h"
+#include "clib/arena.h"
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
@@ -6,7 +9,8 @@
 #include <unistd.h>
 
 #define BACKLOG 10
-#define BUFFER_SIZE 4096 // 4 KB
+#define BUFFER_SIZE 4096           // 4 KB
+#define CONNECTION_ARENA_SIZE 8192 // 8 KB
 
 static int setup_listener(int port) {
   int fd;
@@ -34,20 +38,52 @@ HttpConfig chttp_config_init(int port, const char *pub_dir) {
                       .public_dir = (pub_dir == NULL) ? "." : pub_dir};
 }
 
-void chttp_handle_connection(int client_fd) {
-  char buffer[BUFFER_SIZE] = {0};
+int chttp_handle_connection(int client_fd, const char *pub_dir) {
+  Arena connection_arena;
+  if (arena_init(&connection_arena, CONNECTION_ARENA_SIZE) != 0)
+    return -1;
 
-  ssize_t valread = recv(client_fd, buffer, sizeof(buffer), 0);
-  if (valread <= 0)
-    return;
+  char *buffer = arena_alloc(&connection_arena, BUFFER_SIZE);
+  if (buffer == NULL) {
+    arena_free(&connection_arena);
+    return -1;
+  }
 
-  const char *response =
-      "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-  send(client_fd, response, strlen(response), 0);
+  ssize_t valread = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+  if (valread <= 0) {
+    arena_free(&connection_arena);
+    return -1;
+  }
+
+  HttpRequest *req = chttp_parse_request(buffer, &connection_arena);
+  if (req == NULL) {
+    arena_free(&connection_arena);
+    return -1;
+  }
+  HttpResponse *res = chttp_handle_request(req, pub_dir);
+  if (res == NULL) {
+    chttp_request_free(req);
+    arena_free(&connection_arena);
+    return -1;
+  }
+  size_t res_len = 0;
+  char *raw_res = chttp_serialise_response(res, &res_len);
+  if (raw_res == NULL) {
+    chttp_request_free(req);
+    chttp_response_free(res);
+    arena_free(&connection_arena);
+    return -1;
+  }
+  send(client_fd, raw_res, res_len, 0);
+
+  chttp_request_free(req);
+  chttp_response_free(res);
+  arena_free(&connection_arena);
+  return 0;
 }
 
-int chttp_listen_and_serve(HttpConfig config) {
-  int server_fd = setup_listener(config.port);
+int chttp_listen_and_serve(HttpConfig *config) {
+  int server_fd = setup_listener(config->port);
   if (server_fd < 0)
     return -1;
 
@@ -57,7 +93,7 @@ int chttp_listen_and_serve(HttpConfig config) {
     int client_fd = accept(server_fd, (struct sockaddr *)&address, &addrlen);
 
     if (client_fd >= 0) {
-      chttp_handle_connection(client_fd);
+      chttp_handle_connection(client_fd, config->public_dir);
       close(client_fd);
     }
   }
